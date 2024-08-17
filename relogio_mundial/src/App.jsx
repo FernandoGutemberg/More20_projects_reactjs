f4666b508e5766e3612b62ad1fdc8391f87533c8
FernandoGutemberg <fernando70sil@gmail.com>
2024-08-17 17:20:12 -0300

    Finalizado projeto Relógio Mundial

--- relogio_mundial/src/App.jsx
+++ relogio_mundial/src/App.jsx
@@ -21,18 +21,30 @@ function App() {
     fusoHorarioLocal,
 
   ]);
-  console.log(fusoHorarioLocal);
+  const adicionarFusoHorario = (e) => {
+    const novoFuso = e.target.value;
+    if (!fusosHorariosSelecionados.includes(novoFuso)) {
+      setfusosHorariosSelecionados([...fusosHorariosSelecionados, novoFuso]);
+    }
+  };
 
 
   return (
     <div>
       <h1>Relógio Mundial</h1>
-      <select >
+      <select onChange={(e) => adicionarFusoHorario(e)}>
         <option value="" disabled select>
           Selecione um fuso horário
         </option>
+        {fusosHorarios.map((fuso) => (
+          <option key={fuso} value={fuso}>
+            {fuso}
+
+          </option>
+        )
+        )}
       </select>
-      <div>
+      <div className="separator">
         {fusosHorariosSelecionados.map((fuso) => (
           <TimeZoneCLock key={fuso} timeZone={fuso}/>
         ))}
