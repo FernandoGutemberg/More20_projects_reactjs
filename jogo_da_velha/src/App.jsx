4aa044d4cc076fffb9a7812ec746cb9169ca7114
FernandoGutemberg <fernando70sil@gmail.com>
2024-09-16 23:18:33 -0300

    Projeto jogo da velha

---
+++ jogo_da_velha/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+
+  return (
+    <div>
+      <h1>Jogo da velha</h1>
+      <Board />
+
+    </div>
+
+
+  );
+}
+
+export default App;
