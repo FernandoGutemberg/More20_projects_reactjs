a159e771b5cea9bfb7c901daee60efed4747355b
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-16 22:45:48 -0300

    inserir image

--- gerador_pdf/src/components/GeneratePDF.jsx
+++ gerador_pdf/src/components/GeneratePDF.jsx
@@ -15,19 +15,21 @@ const GeneratePDF = () => {
   const [fontSize, setFontSize] = useState("12");
   const [fontColor, setFontColor] = useState("#000");
   const [isBold, setIsBold] = useState(false);
+  const [image, setImage] = useState(null);
 
 
   const generatePdf = () => {
-   const customStyle = {
-    fontSize: parseInt(fontSize),
-    color: fontColor,
-    bold: isBold,
-   };
+    const customStyle = {
+      fontSize: parseInt(fontSize),
+      color: fontColor,
+      bold: isBold,
+    };
 
     const documentDefinition = {
       content: [
         { text: `Título: ${title}`, style: "customStyle" },
         { text: `Descrição: ${description}`, style: "customStyle" },
+        image ? { image: image, with: 150 } : {},
       ],
       styles: {
         customStyle: customStyle,
@@ -65,7 +67,7 @@ const GeneratePDF = () => {
         isBold={isBold}
         setIsBold={setIsBold} />
 
-      <ImageUpload />
+      <ImageUpload setImage={setImage} />
       <button className='button' onClick={generatePdf}>Gerar PDF</button>
 
     </div>
