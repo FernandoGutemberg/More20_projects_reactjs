2c384016085be94fec0578f7dd2826c2c5e07bd1
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-16 23:00:44 -0300

    preparando ambiente

---
+++ calculadora_impostos/src/components/TaxForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TaxForm() {
+  return (
+    <div>
+        <p>Taxform</p>
+      
+    </div>
+  );
+};
+
+export default TaxForm;
---
+++ calculadora_impostos/src/components/TaxReport.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TaxReport = () => {
+    return (
+        <div>
+            <p>Taxreport</p>
+
+
+        </div>
+    );
+};
+
+export default TaxReport;
