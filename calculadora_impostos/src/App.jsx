8139a7af66ad66e9117222863884092c5b0c14b1
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-17 21:21:41 -0300

    Organizando o app.js

--- calculadora_impostos/src/App.jsx
+++ calculadora_impostos/src/App.jsx
@@ -1,16 +1,24 @@
 import TaxForm from "./components/TaxForm";
 import TaxReport from "./components/TaxReport";
 
+import { Container, Typography } from "@mui/material";
 
 function App() {
 
   return (
-    <>
-    <TaxForm />
-    <TaxReport />
-      
-    </>
-  );
-};
+    <Container style={{ maxWidth: "800px", margin: "0 auto" }}>
+      <Typography
+        variant="h3"
+        align="center"
+        style={{ marginBottom: "16px" }}
+      >
+        Calculadora de Impostos
+      </Typography>
+      <TaxForm />
+      <TaxReport />
 
-export default App
+    </Container>
+  );
+}
+
+export default App;
