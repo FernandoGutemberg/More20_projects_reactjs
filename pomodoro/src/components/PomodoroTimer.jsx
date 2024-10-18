a162d1c375eaf17c2fcdf1616deefff635f23fe6
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-18 20:22:14 -0300

    Realizando interface

--- pomodoro/src/components/PomodoroTimer.jsx
+++ pomodoro/src/components/PomodoroTimer.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+
+
+import {
+    Box,
+    Button,
+    Text,
+    Input,
+    FormControl,
+    FormLabel,
+    Center,
+    HStack
+} from "framer-motion";
 
 const PomodoroTimer = () => {
-  return (
-    <div>
-        <p>PomodoroTimer</p>
-      
-    </div>
-  )
-}
+    return <Center>
+        <Text fontSize="4x1" mb="">10:00</Text>
+
+    </Center>; 
+};
 
 export default PomodoroTimer;
