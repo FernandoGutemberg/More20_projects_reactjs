0d5d2cf9ceb126cf223d0242e37811a8b21cb78b
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-18 19:23:14 -0300

    Finalizando etapa do TaxReport

--- calculadora_impostos/src/components/TaxReport.jsx
+++ calculadora_impostos/src/components/TaxReport.jsx
@@ -7,9 +7,24 @@ const TaxReport = ({ taxData }) => {
             <Typography variant="h5">Relatório de impostos</Typography>
             <Typography variant="body1">Nome:  {taxData.name}</Typography>
             <Typography variant="body1">Idade: {taxData.age}</Typography>
-            <Typography variant="body1">Renda: {parseFloat(taxData.income).toLocaleString("pt-BR", {
-                
-            })}</Typography>
+            <Typography variant="body1">
+                Renda:{" "}
+
+                {parseFloat(taxData.income).toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                })}
+            </Typography>
+            <Typography variant="body1">Imposto a pagar: </Typography>
+            <Typography variant="body1">
+                Renda:{" "}
+
+                {parseFloat(taxData.tax).toLocaleString("pt-BR", {
+                    style: "currency",
+                    currency: "BRL",
+                })}
+            </Typography>
+
 
         </Container>
 
