50e3a318d467672760e5248f6d283a923e0f82c4
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-19 22:08:52 -0300

    primeira logica do pomodoro

--- pomodoro/src/components/PomodoroTimer.jsx
+++ pomodoro/src/components/PomodoroTimer.jsx
@@ -9,21 +9,31 @@ import {
     FormControl,
     FormLabel,
     Center,
-    HStack
-} from "framer-motion";
+    HStack,
+} from "@chakra-ui/react";
 
 const PomodoroTimer = () => {
+    const [minutes, setMinutes] = useState(25);
+    const [seconds, setSeconds] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [userTime, setUserTime] = useState(25);
+
     return (
 
         <Center width="100%" maxWidth="400px" p="4">
             <Box>
 
                 <Text fontSize="4x1" mb="4" textAlign="center">
-                    10:00
+                    {String(minutes).padStart(2, "0")} :{" "}
+                    {String(seconds).padStart(2, "0")}
                 </Text>
                 <FormControl mb="4">
                     <FormLabel>Definir tempo(minutos)</FormLabel>
-                    <Input type="number" />
+                    <Input
+                        type="number"
+                        value={userTime}
+                        onChange={handleChange}
+                        isDisabled={isActive} />
                 </FormControl>
                 <HStack spacing="4">
                     <Button>Iniciar</Button>
