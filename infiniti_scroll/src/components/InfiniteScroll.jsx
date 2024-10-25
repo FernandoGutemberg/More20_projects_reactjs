f7977da6a08a961f1cf97d37ad9735a782e2cfef
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-24 22:30:41 -0300

    setup inicial

---
+++ infiniti_scroll/src/components/InfiniteScroll.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+
+const InfiniteScroll = () => {
+    return (
+        <div>
+            <h1>Infinite Scroll</h1>
+            <ul>
+                <li>
+                    <h3>Título</h3>
+                    <p>Corpo</p>
+                </li>
+            </ul>
+
+        </div>
+    );
+};
+
+export default InfiniteScroll;
