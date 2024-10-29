5cc44307e0eb49248880e24d9e5af984b576b4dd
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-28 21:59:33 -0300

    v1 iniciante compoente tabela informação

--- imc_calc/src/App.jsx
+++ imc_calc/src/App.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react';
+
+import { data } from "./data/data";
+
 import './App.css';
 import ImcCalc from './components/ImcCalc';
+import ImcTable from './components/ImcTable';
+
+
 
 
 function App() {
+  const [imc, setImc] = useState("");
+  const [info, setInfo] = useState("");
+  const [infoClass, setInfoClass] = useState("");
 
   return (
     <div className='container'>
-      <ImcCalc/>
+      {!imc ? (
+        <ImcCalc />
+
+      ) : (
+        <ImcTable />
+
+      )}
     </div>
   );
 };
---
+++ imc_calc/src/components/ImcTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ImcTable() {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default ImcTable;
