5d0e32ddae09ab4cd5f6d85ebd322b8cd1ca2d82
FernandoGutemberg <fernando70sil@gmail.com>
2024-10-29 22:18:12 -0300

    Estruturante componente de tabela

--- imc_calc/src/components/ImcTable.jsx
+++ imc_calc/src/components/ImcTable.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function ImcTable() {
   return (
-    <div>
-      <p>Imc Table</p>
-      
+    <div id="result-container">
+      <p id="imc-number">Seu Imc:</p>
+      <p id="imc-info">Situação atual:</p>
+      <h3>Confira as classificações:</h3>
+      <div id="imc-table"></div>
     </div>
   );
 };
