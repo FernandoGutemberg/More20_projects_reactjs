eff4178d69b7add4c4cb9a2f296357a200071df2
FernandoGutemberg <fernando70sil@gmail.com>
2024-11-03 23:04:27 -0300

    Exibindo o post fim

--- blog/src/routes/Home.jsx
+++ blog/src/routes/Home.jsx
@@ -14,13 +14,13 @@ const Home = () => {
 
     try {
 
-      const response = await axios.get("http://jsonplaceholder.typicode.com/posts"
+      const response = await axios.get("https://jsonplaceholder.typicode.com/posts"
 
       );
 
       const data = response.data;
 
-      console.log(data);
+      setPosts(data);
     } catch (error) {
       console.log(error);
     }
@@ -34,8 +34,15 @@ const Home = () => {
   return (
     <div>
       <h1>Último posts</h1>
-      {posts.length === 0 ? (<p>Carregando...</p>) : <p>Carregou</p>}
-
+      {posts.length === 0 ? (<p>Carregando...</p>) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
     </div>
   )
 };
