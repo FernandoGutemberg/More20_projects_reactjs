fbcaf1e5f3168804eddd742dde0d46e2332f8839
FernandoGutemberg <fernando70sil@gmail.com>
2024-11-26 22:08:39 -0300

    pagina de criacao de POST

--- blog/src/routes/NewPost.jsx
+++ blog/src/routes/NewPost.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import "./NewPost.css";
 
 const NewPost = () => {
-  return (
-    <div>
-        <h1>NewsPost</h1>
-      
-    </div>
-  )
+  return <div className="new-post">
+    <h2>Inserir novo Post:</h2>
+    <form action="">
+      <div className="form-control">
+        <label htmlFor="title">Título:</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          placeholder="Digite o título"
+        />
+
+      </div>
+
+      <div className="form-control">
+        <label htmlFor="body">Conteúdo:</label>
+        <textarea
+          name="body"
+          id="body"
+          placeholder="Digite o conteúdo"
+        ></textarea>
+      </div>
+      <input type="submit" value="Criar Post" className="btn" />
+    </form>
+  </div>;
 };
 
 export default NewPost;
