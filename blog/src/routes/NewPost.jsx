0bd880a3534d25a49b0709bd84efd90a84e8f9a2
FernandoGutemberg <fernando70sil@gmail.com>
2024-12-03 23:02:20 -0300

    alter

--- blog/src/routes/NewPost.jsx
+++ blog/src/routes/NewPost.jsx
@@ -14,33 +14,47 @@ const NewPost = () => {
   const [title, setTitle] = useState();
   const [body, setBody] = useState();
 
+  const createPost = async (e) => {
+    e.preventDefault();
 
+    const post = { title, body, userId: 1 };
 
-  return <div className="new-post">
-    <h2>Inserir novo Post:</h2>
-    <form action="">
-      <div className="form-control">
-        <label htmlFor="title">Título:</label>
-        <input
-          type="text"
-          name="title"
-          id="title"
-          placeholder="Digite o título"
-        />
+    await blogFetch.post("/posts", {
+      body: post,
+    });
+  };
 
-      </div>
+  return (
 
-      <div className="form-control">
-        <label htmlFor="body">Conteúdo:</label>
-        <textarea
-          name="body"
-          id="body"
-          placeholder="Digite o conteúdo"
-        ></textarea>
-      </div>
-      <input type="submit" value="Criar Post" className="btn" />
-    </form>
-  </div>;
+    <div className="new-post">
+      <h2>Inserir novo Post:</h2>
+      <form onSubmit={() => createPost()}>
+        <div className="form-control">
+          <label htmlFor="title">Título:</label>
+          <input
+            type="text"
+            name="title"
+            id="title"
+            placeholder="Digite o título"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea
+            name="body"
+            id="body"
+            placeholder="Digite o conteúdo"
+            onChange={(e) => setBody(e.target.value)}
+
+          ></textarea>
+        </div>
+        <input type="submit" value="Criar Post" className="btn" />
+      </form>
+    </div>
+  );
 };
 
 export default NewPost;
