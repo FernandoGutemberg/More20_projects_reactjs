dc1b750ab3628d84363ddfe4e56b9a0243183528
FernandoGutemberg <fernando70sil@gmail.com>
2024-12-19 17:41:05 -0300

    Inserindo component gerenciar

--- blog/src/routes/Post.jsx
+++ blog/src/routes/Post.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import blogFetch from "../axios/config";
+import { useParams } from "react-router-dom";
+
+import "./Post.css";
 
 const Post = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+    const { id } = useParams();
+    const [post, setPost] = useState({});
 
-export default Post
+    const getPost = async() => {
+        try {
+            console.log(id);
+            const response = await blogFetch.get(`/posts/${id}`);
+
+            const data = response.data;
+
+            setPost(data);
+
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getPost();
+    }, []);
+
+
+
+    return (
+        <div className="post-container">
+            {!post.title ? (
+                <p>Carregando...</p>
+            ) : (
+                <div className="post">
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+
+                </div>
+            )}
+        </div>
+
+    );
+};
+
+export default Post;
