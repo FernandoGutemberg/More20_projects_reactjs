177129911e680de467ed37b43b2f7141e506fc72
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-03 21:46:54 -0300

    Corrigindo referências para o diretório mariah

---
+++ mariah/src/data/questions_complete.js
@@ -0,0 +1,40 @@
+const data = [
+     {
+      category: "Sobre Mariah",
+      questions: [
+        {
+          question: "Qual o prato favorito?",
+          options: [
+            "Macarrão",
+            "Feijoada",
+            "Costela e arroz",
+            "Strogonoff de frango",
+          ],
+          answer: "Macarrão",
+        },
+        {
+          question: "Série preferida?",
+          options: ["Dorama", "A Turma da Mônica", "Breaking Bad ", "Chaves"],
+          answer: "Dorama",
+        },
+        {
+          question: "Qual segundo idioma preferido ? :) kkkkkk",
+          options: ["Latim", "Espanhol", "Mandarin", "Inglês"],
+          answer: "Inglês",
+        },
+        {
+          question: "Qual a data de aniversário dela?",
+          options: ["01/05", "30/01", "27/4", "05/10"],
+          answer: "01/05",
+        },
+        {
+          question:
+            "Qual a cor preferida de flor ?",
+          options: ["Rosa", "Vermelho", "Branco", "Amarelo"],
+          answer: "Vermelho",
+        },
+      ],
+    },
+  ];
+  
+  export default data;
