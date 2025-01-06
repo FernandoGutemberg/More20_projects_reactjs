c15bc951b891d26321b2d86427c5e5abca3fbd4d
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-05 23:49:38 -0300

    Estrutura do componente principal

--- formulario_multistep_react/src/App.jsx
+++ formulario_multistep_react/src/App.jsx
@@ -1,12 +1,40 @@
+// Components
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
+
+
 import './App.css';
 
 function App() {
 
   return (
-    <div className='App'>
-      <h1>React Multistep</h1>
+    <div className='app'>
+      <div className='header'>
+        <h1>React Multistep</h1>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>Etapas</p>
+        <form action="">
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            
+            </button>
+          </div>
+        </form>
+
+      </div>
     </div>
-    
+
   );
 };
 
