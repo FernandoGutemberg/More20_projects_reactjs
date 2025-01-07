b6a82c0c67ce76e4ce9ac47fa73ecf18d4d1bf0d
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-06 23:06:23 -0300

    funcionalidade de mudar de mapa

--- formulario_multistep_react/src/App.jsx
+++ formulario_multistep_react/src/App.jsx
@@ -1,9 +1,11 @@
 // Components
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
+import { FiSend } from 'react-icons/fi';
 import UserForm from './components/UserForm';
 import ReviewForm from './components/ReviewForm';
 import Thanks from './components/Thanks';
 
+// Hooks
 import { useForm } from './hooks/useForm';
 
 import './App.css';
@@ -12,9 +14,7 @@ function App() {
 
   const formComponents = [<UserForm />, <ReviewForm />, <Thanks />];
 
-  const { currentStep, currentComponent} = useForm(formComponents)
-
-
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents);
 
   return (
     <div className='app'>
@@ -26,19 +26,32 @@ function App() {
       </div>
       <div className="form-container">
         <p>Etapas</p>
-        <form action="">
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type='button'>
-              <GrFormPrevious />
-              <span>Voltar</span>
+            {!isFirstStep && (
+              <button type='button' onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
 
-            </button>
-            <button type='submit'>
-              <span>Avançar</span>
-              <GrFormNext />
+              </button>
+            )}
 
-            </button>
+
+
+            {!isLastStep ? (
+              <button type="button" onClick={() => changeStep(currentStep + 1)}>
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+
+            ) : (
+              <button type='button'>
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
+
           </div>
         </form>
 
