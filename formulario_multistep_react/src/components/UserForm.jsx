6d825758906c0e45d753cdfca3e704d5e43dbff7
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-07 22:12:45 -0300

    EStilos iniciais

--- formulario_multistep_react/src/components/UserForm.jsx
+++ formulario_multistep_react/src/components/UserForm.jsx
@@ -3,8 +3,28 @@ import React from 'react';
 const UserForm = () => {
   return (
     <div>
-        <h1>User Form</h1>
-      
+      <h2>User Form</h2>
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          name="text"
+          id="name"
+          placeholder="Digite o seu nome"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o seu e-mail"
+          required
+        />
+      </div>
+
     </div>
   );
 };
