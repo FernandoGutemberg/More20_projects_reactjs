1e938b7614efe2842ed170a7d8c803f1bfb18113
FernandoGutemberg <fernando70sil@gmail.com>
2025-01-29 14:11:05 -0300

    component de envio de avaliação

--- formulario_multistep_react/src/components/Thanks.jsx
+++ formulario_multistep_react/src/components/Thanks.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import "./Thanks.css";
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from 'react-icons/bs';
 
 const Thanks = () => {
   return (
-    <div>
-        <h2>Thanks</h2>
-      
+    <div className="thanks-container">
+      <h2>Falta pouco....</h2>
+      <p>
+        A sua opnicão é muito importante, em breva você receberá um cupom de 10%
+        de desconto para a sua próxima compra.
+      </p>
+      <p>Para concluir a sua avaliação clique no botão de Enviar abaixo.</p>
+      <h3>Aqui está o resumo da sua avalização:</h3>
+      <p className='review-data'>
+        <span>Satisfação com o produto:</span>
+      </p>
+      <p className='review-data'>
+        <span>Comentário:</span>
+      </p>
+
+
+
+
     </div>
   );
 };
