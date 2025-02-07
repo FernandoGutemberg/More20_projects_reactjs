d3319545398f90eff183f3876e77bcf4cc42a52a
FernandoGutemberg <fernando70sil@gmail.com>
2025-02-07 14:57:59 -0300

    pt1 component de comentároi

--- formulario_multistep_react_typescript/src/components/ReviewForm.tsx
+++ formulario_multistep_react_typescript/src/components/ReviewForm.tsx
@@ -1,11 +1,55 @@
+import {
+  BsFillEmojiHeartEyesFill,
+  BsFillEmojiSmileFill,
+  BsFillEmojiNeutralFill,
+  BsFillEmojiFrownFill,
+} from 'react-icons/bs';
 
-const ReviewForm = () => {
+type ReviewForm = {
+  data: {
+    review: string;
+    comment: string;
+  };
+  updateFielHandler: (key: string, value: string) => void;
+};
+
+import "./ReviewForm.css";
+
+
+const ReviewForm = ({ data, updateFielHandler }: ReviewForm) => {
   return (
-    <div>
-        ReviwForm
-      
-    </div>
-  )
-}
+    <div className='review-form'>
+      <div className="form-control score-container">
+        <label className="radio-container">
+          <input type="radio" value="unsatisfied" name="review" required />
+          <BsFillEmojiFrownFill />
+          <p>Insatisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="neutral" name="review" required />
+          <BsFillEmojiNeutralFill />          
+          <p>Poderia ser melhor</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="satisfied" name="review" required />
+          <BsFillEmojiSmileFill />
+          <p>Satisfeito</p>
+        </label>
+        <label className="radio-container">
+          <input type="radio" value="very_satisfied" name="review" required />
+          <BsFillEmojiHeartEyesFill />
+          <p>Muito Satisfeito</p>
+        </label>
+      </div>
+      <div className="form-control">
+        <label htmlFor="comment">Comentário:</label>
+        <textarea name="comment" id="comment" placeholder="Conte como foi a sua experiência com o produto..." required></textarea>
 
-export default ReviewForm
+      </div>
+
+
+    </div>
+  );
+};
+
+export default ReviewForm;
