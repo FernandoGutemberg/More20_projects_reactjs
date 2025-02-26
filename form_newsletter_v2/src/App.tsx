cb5b8c9f8150ad23677a87b497d84817b322d7a7
FernandoGutemberg <fernando70sil@gmail.com>
2025-02-26 14:30:45 -0300

    criando elementos

--- form_newsletter_v2/src/App.tsx
+++ form_newsletter_v2/src/App.tsx
@@ -2,14 +2,17 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <>
+    <div>
       <h1 className="text-3xl font-bold text-blue-500">Inscreva-se</h1>
-      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-        Cadastrar
-      </button>
+      <p>Assine nossa Newsletter e mantenha-se informado</p>
+      <div>
+        <Form />
+      </div>
+      <p>
+        Ao se inscrever, você passará a receber os nossos e-mails com as melhores dicas, novidades e ofertas.
+      </p>
 
-      <Form />
-    </>
+    </div>
   );
 }
 
--- form_newsletter_v2/src/components/Form.tsx
+++ form_newsletter_v2/src/components/Form.tsx
@@ -1,10 +1,35 @@
 
 const Form = () => {
-  return (
-    <div> Form
-      
-    </div>
-  );
+    return (
+        <form>
+            <div>
+                <label>Nome</label>
+                <input type="text" placeholder="Digite seu nome" />
+
+            </div>
+            <div>
+                <label>E-mail</label>
+                <input type="email" placeholder="Insira seu melhor e-mail" />
+
+            </div>
+            <div>
+                <a href="#">Leia os termos</a>
+                <div>
+                    <input type="checkbox" />
+                    <label>Concordo com os termos</label>
+
+
+
+                </div>
+            </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Cadastrar
+            </button>
+
+
+
+        </form>
+    );
 }
 
 export default Form;
