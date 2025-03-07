19a35070d07919575a7ff2403bfa926a8a843adc
FernandoGutemberg <fernando70sil@gmail.com>
2025-03-07 02:02:58 -0300

    setup inicial

---
+++ react_hook_form_/src/App.tsx
@@ -0,0 +1,15 @@
+import Form from './components/Form';
+
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">React hook form Typescript</h1>
+      <Form></Form>
+
+
+    </div>
+  );
+}
+
+export default App;
