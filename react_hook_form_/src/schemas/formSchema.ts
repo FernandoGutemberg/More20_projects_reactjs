924f52a36405c0955b71be302d76959ec5f28db9
FernandoGutemberg <fernando70sil@gmail.com>
2025-03-18 16:30:53 -0300

    Atualizando projeto 18_03

--- react_hook_form_/src/schemas/formSchema.ts
+++ react_hook_form_/src/schemas/formSchema.ts
@@ -1,6 +1,13 @@
-import {z} from 'zod';
+import { z } from 'zod';
 
 export const formSchema = z.object {(
-    name: z.string().noneempty('Campo obrigatório').min('3')
+    name: z.string().min('3', "O mínimo de caracteres é 3."),
+        lastname: z.string().min(3, "O número mínimo de caracteres é 3."),
+            gender: z.string().refine((field) => field !== "select", {
+                message: "Você precisa escolher um gênero"
 
-)}
+            })
+            email: z.string().min(1, "O campo é obrigatório")
+    
+
+)};
