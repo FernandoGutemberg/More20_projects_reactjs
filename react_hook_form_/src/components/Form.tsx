0554ebb5667337f57c1768bbbcfcd84a2438d7f7
FernandoGutemberg <fernando70sil@gmail.com>
2025-03-21 23:54:15 -0300

    validacao de senha e checkbox

--- react_hook_form_/src/components/Form.tsx
+++ react_hook_form_/src/components/Form.tsx
@@ -78,6 +78,54 @@ const Form = () => {
         )}
       </div>
 
+      <div className="flex flex-col gap-2 mb-2">
+        <label htmlFor="password">Senha:</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Digite a sua senha"
+          {...register("password")}
+          className="h-10 pl-2 rounded-md border border-slate-300"
+        />
+        {errors.password && (<small
+          className="text-red-500 italic">{errors.password.message}</small>
+
+        )}
+      </div>
+
+      <div className="flex flex-col gap-2 mb-2">
+        <label htmlFor="confirmpassword">Confirmação de senha:</label>
+        <input
+          type="password"
+          id="confirmpassword"
+          placeholder="Confirme a sua senha"
+          {...register("confirmpassword")}
+          className="h-10 pl-2 rounded-md border border-slate-300"
+        />
+        {errors.confirmpassword && (<small
+          className="text-red-500 italic">{errors.confirmpassword.message}</small>
+
+        )}
+      </div>
+
+      <div className="flex flex-col gap-2 mb-2">
+        <div className="flex gap-2">
+          <input
+            type="checkbox"
+            id="agree"
+            placeholder="Confirme a sua senha"
+            {...register("agree")}
+          />
+          <label htmlFor="agree">Confirmação com os termos</label>
+
+        </div>
+
+
+        {errors.agree && (<small
+          className="text-red-500 italic">{errors.agree.message}</small>
+
+        )}
+      </div>
 
 
 
